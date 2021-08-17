26de86c8cd5baeb21deec5b855ac440d31939918
hk0305 <hyounkeun2009@gmail.com>
2021-08-17 15:56:10 +0900

    Add BaseComponent

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,17 @@
-import { PageCompoent } from "./components/page.js";
+import { ImageComponent } from "./components/page/item/image.js";
+import { PageCompoent } from "./components/page/page.js";
 
 class App {
   private readonly page: PageCompoent;
   constructor(appRoot: HTMLElement) {
     this.page = new PageCompoent();
     this.page.attachTo(appRoot);
+
+    const image = new ImageComponent(
+      "Image Title",
+      "https://picsum.photos/600/300"
+    );
+    image.attachTo(appRoot, "beforeend");
   }
 }
 
