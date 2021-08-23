1429e13350e0504e8905d589b60ecb8ee2aed108
hk0305 <hyounkeun2009@gmail.com>
2021-08-23 18:34:20 +0900

    * 소스 동기화

--- src/components/page/page.ts
+++ src/components/page/page.ts
@@ -1,33 +1,24 @@
-import { BaseComponent, Component } from "../component.js";
+import { BaseComponent, Component } from './../component.js';
 
 export interface Composable {
   addChild(child: Component): void;
 }
 
-export class PageItemComponent
-  extends BaseComponent<HTMLElement>
-  implements Composable
-{
+class PageItemComponent extends BaseComponent<HTMLElement> implements Composable {
   constructor() {
     super(`<li class="page-item">
-              <section class="pate-item__body"></section>
-              <div class="page-item__controls">
-                <button class="close">&times;</button>
-              </div>
-           </li>`);
+            <section class="page-item__body"></section>
+            <div class="page-item__controls">
+              <button class="close">&times;</button>
+            </div>
+          </li>`);
   }
   addChild(child: Component) {
-    const container = this.element.querySelector(
-      ".page-item__body"
-    )! as HTMLElement;
+    const container = this.element.querySelector('.page-item__body')! as HTMLElement;
     child.attachTo(container);
   }
 }
-
-export class PageCompoent
-  extends BaseComponent<HTMLUListElement>
-  implements Composable
-{
+export class PageComponent extends BaseComponent<HTMLUListElement> implements Composable {
   constructor() {
     super('<ul class="page"></ul>');
   }
@@ -35,6 +26,6 @@ export class PageCompoent
   addChild(section: Component) {
     const item = new PageItemComponent();
     item.addChild(section);
-    item.attachTo(this.element, "beforeend");
+    item.attachTo(this.element, 'beforeend');
   }
 }
